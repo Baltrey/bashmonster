d31c644c91dc7c76f69bda7c53216c0d70bc9336
Baltazar Biverstal <baltazar.biverstal@elev.ga.ntig.se>
2023-11-17 11:07:39 +0100

    update

---
+++ bashamonster/player.cs
@@ -0,0 +1,14 @@
+public class player
+{
+    int HP = 100;
+    int Attack;
+    string Name;
+
+}
+public class item
+{
+    string Name;
+    int Damage;
+    int Durability;
+    bool Instakill = false;
+}
